cabaffcfd5e4c3e038797bf75b6180058e9cfff5
ChayanutP <chayanut.pimpramote@gmail.com>
2025-08-21 16:02:33 +0700

    Add StudentTest.java

---
+++ src/test/java/ku/cs/models/StudentTest.java
@@ -0,0 +1,45 @@
+package ku.cs.models;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+    Student s;
+    @BeforeEach
+    void init(){
+        s = new Student("6710405273", "Test");
+    }
+
+    @Test
+    void testChangeName() {
+        s.changeName("TestMe");
+        assertEquals("TestMe", s.getName());
+
+    }
+
+    @Test
+    @DisplayName("ทดสอบการเพิ่มคะแนน")
+    void testAddScore() {
+        s.addScore(58.5);
+        assertEquals(58.5, s.getScore());
+    }
+    @Test
+    void testCalculateGrade() {
+        s.addScore(20);
+        assertEquals("F", s.grade());
+    }
+
+    @Test
+    void testIsID(){
+        assertTrue(s.isId("6710405273"));
+    }
+
+    @Test
+    void testIsNameContains(){
+        assertTrue(s.isNameContains("est"));
+    }
+}
